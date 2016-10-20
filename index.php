1106d445c35faf664834d26edac9383aff483042
Gerard Konan <gerard.konan@agilly.net>
2016-10-20 19:55:00 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<table style="width: 600px; margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<td>
+			<table style="margin-left: auto; margin-right: auto;">
+				<tbody>
+					<tr>
+						<td style="text-align: justify;"><img alt="" height="79" src="http://oh6h.mj.am/img/oh6h/b/1uyv3/xgoln.jpeg" width="200" /></td>
+						<td style="text-align: center;">
+						<p style="text-align: justify;"><img alt="" height="60" src="http://oh6h.mj.am/img/oh6h/b/1uyv3/xgolg.png" width="250" /></p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<h2 style="text-align: justify;"><img alt="" height="124" src="http://oh6h.mj.am/img/oh6h/b/1uyv3/xgol4.png" width="600" /></h2>
+
+			<h2 style="text-align: center;"><span style="font-family:lucida sans unicode,lucida grande,sans-serif;"><span style="color: rgb(128, 128, 128);"><strong>PME/PMI, Augmentez votre comp&eacute;titivit&eacute; &agrave; l&#39;aide du Cloud</strong></span></span></h2>
+
+			<p style="text-align: center;"><span style="font-family:lucida sans unicode,lucida grande,sans-serif;"><strong>Vendredi 28 Octobre 2016 </strong>&agrave; 9h, </span><span style="font-family:lucida sans unicode,lucida grande,sans-serif;">Microsoft West and Central Africa, 6eme Etage, Imm Broadway, Avenue Nogues, Plateau.</span></p>
+
+			<p style="text-align: center;"><span style="font-family:lucida sans unicode,lucida grande,sans-serif;"><span style="font-size:14px;">Au cours de ce s&eacute;minaire, nous d&eacute;montrerons comment transformer la technologies en avantage commercial et accroitre votre comp&eacute;titivit&eacute;.</span></span></p>
+
+			<p style="text-align: center;"><span style="font-size:12px;"><span style="font-family: lucida sans unicode,lucida grande,sans-serif;">Tel : +225 22 42 27 39 - email : infos@agilly.net</span></span></p>
+
+			<p style="text-align: justify;"><iframe frameborder="0" height="515" marginheight="0" marginwidth="0" scrolling="no" src="https://app.mailjet.com/widget/iframe/1jR9/1Li" width="600"></iframe></p>
+			</td>
+		</tr>
+	</tbody>
+</table>
